955b2fa77e7183fe788097f93bc10988e45e99a5
Cara McKinley <cmckinley@thisisdk.com>
2012-10-18 18:27:47 -0700

    add snippets for moving panels left to right on page transitions; port over some css from old phonegap app, css tweaks

--- www/app/scripts/routes/app-router.js
+++ www/app/scripts/routes/app-router.js
@@ -11,37 +11,88 @@
  */
 
 define([
-    'jquery',
-    'underscore',
-    'backbone'
+	'jquery',
+	'underscore',
+	'backbone'
 ], function($, _, Backbone){
 	var AppRouter = Backbone.Router.extend({
 
 		routes: {
-	      '': 'home',
-	      'crime-list': 'crimeList',
-	      // Default
-	      '*actions': 'defaultAction'
-	    },
+		  '': 'showHome',
+		  'crime-list': 'showCrimeList'
+		},
 
-	    initialize: function(){
+		initialize: function(){
 			console.log('init');
 
-			this.on('route:home', _.bind(this.showHome, this));
-			this.on('route:crimeList', _.bind(this.showCrimeList, this));
-	    },
+			this.pageHistory = []; // store page history to determine left/right panel action
+			this.currentPage = null;
+		},
 
-	    showHome: function(){
+		showHome: function(){
 			console.log('showing home');
-	    },
+		},
 
-	    showCrimeList: function(){
+		showCrimeList: function(){
 			console.log('showing crimes');
-	    },
+		},
 
-	    defaultAction: function(){
+		defaultAction: function(){
 
-	    }
+		},
+
+		/**
+		 * page slide action, determines if pages should slide left or right
+		 * borrowed from https://github.com/ccoenraets/backbone-directory/blob/master/iphone/js/app.js
+		 * @param  {object} page [backbone view of page]
+		 */
+		slidePage: function(page) {
+			var slideFrom,
+				self = this;
+
+			if (!this.currentPage) {
+				// If there is no current page (app just started) -> No transition: Position new page in the view port
+				$(page.el).attr('class', 'page stageCenter');
+				$('#content').append(page.el);
+				this.pageHistory = [window.location.hash];
+				this.currentPage = page;
+				return;
+			}
+
+			// Cleaning up: remove old pages that were moved out of the viewport
+			$('.stageRight, .stageLeft').not('#searchPage').remove();
+
+			if (page === this.homePage) {
+				// Always apply a Back (slide from left) transition when we go back to the home page
+				slideFrom = 'left';
+				$(page.el).attr('class', 'page stageLeft');
+				this.pageHistory = [window.location.hash];
+
+			} else if (this.pageHistory.length > 1 && window.location.hash === this.pageHistory[this.pageHistory.length - 2]) {
+				// The new page is the same as the previous page -> Back transition
+				slideFrom = 'left';
+				$(page.el).attr('class', 'page stageLeft');
+				this.pageHistory.pop();
+
+			} else {
+				// Forward transition (slide from right)
+				slideFrom = 'right';
+				$(page.el).attr('class', 'page stageRight');
+				this.pageHistory.push(window.location.hash);
+			}
+
+			$('#content').append(page.el);
+
+			// Wait until the new page has been added to the DOM...
+			setTimeout(function() {
+				// Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
+				$(self.currentPage.el).attr('class', 'page transition ' + (slideFrom === 'right' ? 'stageLeft' : 'stageRight'));
+				// Slide in the new page
+				$(page.el).attr('class', 'page stageCenter transition');
+				self.currentPage = page;
+			});
+
+		}
 
 	});
 
